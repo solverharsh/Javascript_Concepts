814c19e81188aaea18fbc80463157feb57fa3cca
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2023-12-23 21:02:21 +0000

    data types snippet

---
+++ JS_Snippets/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version/JS;
+
+//alert("Hello");   // We are using nodejs ,not browser so it will give error;
+
+let name ="Prakash";
+let age =20;
+let flag =false;
+let state =null;
+
+//DataType:
+//number      =>
+//bigint      => It is used for mostly massive data;
+//string      =>"";
+//boolean     =>true/false;
+//null        =>standalone value;
+//undefined   =>                 ;
+//symbol      =>unique         ;
+
+
+//object
+
+console.log(typeof name);    //string
+console.log(typeof state);   //object
+console.log(typeof null);    //object
