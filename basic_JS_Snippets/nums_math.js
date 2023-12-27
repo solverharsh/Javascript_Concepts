738aa1ccd16f03d9bd2b7272afa6b250ffcd2b9e
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2023-12-27 11:41:19 +0000

    removed comments

--- basic_JS_Snippets/nums_math.js
+++ basic_JS_Snippets/nums_math.js
@@ -1,4 +1,4 @@
-/*const balance =new Number(100)
+const balance =new Number(100)
 console.log(balance);
 
 console.log(balance.toString().length);  //It got converted to String;
@@ -10,13 +10,13 @@ console.log(otherNumber.toPrecision(5));
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN'));
-*/
+
 //++++++++++++++++++++++++MATH+++++++++++++++++++++++++++++
-//  console.log(Math)
-//  console.log(Math.abs(-4));
-//  console.log(Math.round(4.6));
-//  console.log(Math.ceil(4.2));
-//  console.log(Math.floor(4.9));
+ console.log(Math)
+ console.log(Math.abs(-4));
+ console.log(Math.round(4.6));
+ console.log(Math.ceil(4.2));
+ console.log(Math.floor(4.9));
 
 console.log(Math.random());             //Values between 0 & 1;
 console.log((Math.random()*10)+1);   
