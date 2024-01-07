1f30fcf60a03d753ec09dca3c822263481f0977e
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2024-01-07 18:18:01 +0000

    added conditional snippets

---
+++ control_flow/first.js
@@ -0,0 +1,12 @@
+// if condition :
+
+if(true){
+    console.log("Welcome to conditional statement")
+}
+
+const isUserLoggedIn =true
+
+if(isUserLoggedIn){
+    console.log("User is Logged in");
+}
+
