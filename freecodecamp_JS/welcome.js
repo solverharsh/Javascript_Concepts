d6c015e8029364e83093580f94cb8267d9d8b8b4
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2024-09-30 20:47:01 +0000

    change of folder name and structure

--- freecodecamp_JS/welcome.js
+++
@@ -1,8 +0,0 @@
-
-let myName= "Zoe";
-let greeting = "Hi Sir/Maam ,I am ";
-
-// let welcome= greeting + myName;
-// console.log(welcome);
-
-document.getElementById("welcome-el").innertext = greeting + myName;
