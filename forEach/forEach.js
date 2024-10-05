4a4d99e4aac000b40d686f2a6c9b6e368cd62711
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2024-10-05 18:54:05 +0000

    added forEach concept code

---
+++ forEach/forEach.js
@@ -0,0 +1,19 @@
+// let arr = [1,2,3,4,5];
+
+// arr.forEach((ele)=>{
+//     console.log(ele);
+// })
+
+// ----------------------Not possible on Objects ;
+
+// let obj = {
+//     name : 'Prashant',
+//     city:'UP',
+//     profession :'Teacher',
+//     Income : '1 lakh',
+// }
+
+// obj.forEach((ele)=>{
+//     console.log(ele);
+    
+// })
