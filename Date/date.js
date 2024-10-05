08d5365c71a9725bd64f9b92dd79db39101894ac
Harshdeep kumar <126336975+solverharsh@users.noreply.github.com>
2024-10-05 07:30:17 +0000

    added Date folder with few date fns

---
+++ Date/date.js
@@ -0,0 +1,26 @@
+let myDate= new Date();
+console.log(myDate);
+console.log(myDate.getDate());
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getMonth());
+console.log(myDate.getHours());
+
+/*
+.................................OUTPUT :.............................
+
+@solverharsh ➜ /workspaces/Javascript_Concepts/Date (main) $ node date.js
+2024-10-05T07:28:11.805Z
+5
+1728113291805
+2024
+7
+805
+9
+7
+
+*/
+
+
